37091699f75c767bae1bce009e12733fc8947e3d
yukimi.yamada <yukimi2356@gmail.com>
2022-03-13 13:14:58 +0900

    ページネーションを追加

--- src/main/java/models/Task.java
+++ src/main/java/models/Task.java
@@ -14,10 +14,15 @@ import javax.persistence.Table;
 @Entity
 @NamedQueries({
     @NamedQuery(
-        name = "getAllTasks",
-        query = "SELECT t FROM Task AS t ORDER BY t.id DESC"
-    )
+            name = "getAllTasks",
+            query = "SELECT t FROM Task AS t ORDER BY t.id DESC"
+            ),
+    @NamedQuery(
+            name = "getTasksCount",
+            query = "SELECT COUNT(t) FROM Task AS t"
+            )
 })
+
 @Table(name = "tasks")
 public class Task {
     @Id
